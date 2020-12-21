281842f559fa2aae28f97a8f3251c0328d4127b7
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2020-12-21 12:12:08 -0300

    Change the length of VARCHAR date

--- server/db/db.sql
+++ server/db/db.sql
@@ -6,14 +6,14 @@ CREATE TABLE income (
 	id INT(11) NOT NULL AUTO_INCREMENT,
 	concept VARCHAR(45) DEFAULT NULL,
     amount INT(11) NOT NULL,
-    date VARCHAR(10) NOT NULL,
+    date VARCHAR(20) NOT NULL,
     PRIMARY KEY(id)
 );
 CREATE TABLE expenses (
 	id INT(11) NOT NULL AUTO_INCREMENT,
 	concept VARCHAR(45) DEFAULT NULL,
     amount INT(11) NOT NULL,
-    date VARCHAR(10) NOT NULL,
+    date VARCHAR(20) NOT NULL,
     PRIMARY KEY(id)
 );
 
