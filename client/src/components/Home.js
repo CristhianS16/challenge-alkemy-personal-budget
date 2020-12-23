54f1f508406baf2cc5cedd93409b232bb4678062
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2020-12-23 17:51:07 -0300

    Finish the view of Home, add functions to last movements and balance.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState} from "react";
 import { Table } from "react-bootstrap";
+import Error from "./Error";
+import Spinner from "./Spinner";
+import {getTotal} from "../helpers/getDataHome";
 
-const Home = ({ setTitle, totalBudget }) => {
-  console.log(totalBudget);
+const Home = (props) => {
+
+  const {setTitle, totalBudget, setTotalBudget, viewSpinner, setViewSpinner, viewError, setViewError} = props;
+
+  const [lastMovements, setLastMovements] = useState([]);
 
   useEffect(() => {
     setTitle("Home");
-  });
+    setViewSpinner(true);
+
+    async function getDataHome () {
+      try {
+        const {totalIncome, totalExpenses, movements} = await getTotal();
+
+        setTotalBudget(totalIncome - totalExpenses);
+        setLastMovements(movements);
+        setViewSpinner(false);
+        setViewError(false);    
+      } catch (error) {
+        console.log(error);
+        setViewSpinner(false);
+        setViewError(true);
+      };
+    };
+    
+    getDataHome();
+  }, [setTitle, setTotalBudget, setViewError, setViewSpinner]);
+
 
   return (
     <div className="container col-10 py-3 mt-2">
@@ -16,7 +41,9 @@ const Home = ({ setTitle, totalBudget }) => {
         </div>
         <div className="row">
           <hgroup className="col-11 border rounded bg-white d-flex flex-column flex-md-row align-items-md-baseline mx-auto">
-            <h4 className="text-info mt-2">Total budget:</h4>
+            <h4 className={
+              totalBudget > 0 ? "text-success mt-2" : totalBudget === 0 ? "text-info mt-2" : "text-danger mt-2"
+            }>Total budget:</h4>
             <h3 className="font-weight-bold ml-1 font-size-2 ml-md-3">
               $ {totalBudget}
             </h3>
@@ -25,7 +52,7 @@ const Home = ({ setTitle, totalBudget }) => {
             <h3>Last Movements</h3>
             <div>
               <Table striped bordered hover variant="light" size="sm">
-                <thead>
+                <thead className="thead-dark">
                   <tr>
                     <th>Concept</th>
                     <th>Amount</th>
@@ -33,58 +60,25 @@ const Home = ({ setTitle, totalBudget }) => {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
-                  <tr>
-                    <td>Salary</td>
-                    <td>$ 1000</td>
-                    <td>10-12-2020</td>
-                  </tr>
+                  {
+                    lastMovements.map( (movement, i) => (
+                      <tr key={i} className={
+                       movement.type === 'income' ? "table-success" : "table-danger"
+                      }>
+                        <td>{movement.concept}</td>
+                        <td>$ {movement.amount}</td>
+                        <td>{movement.date}</td>
+                      </tr>
+                    ))
+                  }
                 </tbody>
               </Table>
+            </div>
+            <div className="row pb-3">
+            {viewSpinner ? <Spinner /> : null}
+            {viewError ? <Error 
+              msgError = "Could not connect to the database."
+            />: null}
             </div>
           </div>
         </div>
