7ce20fbe5244b7f6530701f57200919807f3725f
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2020-12-23 17:46:49 -0300

    Add a footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="row">
+          <footer className="col-12 d-flex align-items-center justify-content-center bg-dark text-white py-3">
+            <p className="m-0">&#169; Challenge from Alkemy: Created by <span className="text-primary">Strokol Cristhian Daniel</span></p>
+          </footer>
+        </div>
+    )
+}
+
+export default Footer;
