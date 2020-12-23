1663cbc7cc84ee5c5897be3f5fe398cf50cf87ee
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2020-12-23 17:34:37 -0300

    Rename Navbar component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import {Navbar, Nav} from 'react-bootstrap';
+
+const Navigation = ({title}) => {
+    return (
+        <Navbar animation="false" collapseOnSelect expand="lg" bg="primary" variant="dark" fixed="top">
+          <Navbar.Brand>{title}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse animation="false" id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to ="operations">Operations</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Navigation;
