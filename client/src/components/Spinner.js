4e86fa1fbd80192f03c7ef508be6acca0f1973aa
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2020-12-23 17:49:45 -0300

    Add a Spinner component

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spinner = () => {
+    return (
+        <div className="sk-chase mx-auto">
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+        </div>
+    )
+}
+
+export default Spinner
